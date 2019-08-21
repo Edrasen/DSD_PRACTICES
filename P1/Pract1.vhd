5f31272560023520daa94aa477acc3728202b4d0
Edgar Alain Ramos Mesas <alain42117@hotmail.com>
2019-08-21 01:18:47 -0500

    Update Pract1.vhd

--- P1/Pract1.vhd
+++ P1/Pract1.vhd
@@ -12,33 +12,31 @@ end ff;
 
 architecture aff of ff is
 	SIGNAL qsr, qjk, qd, qt, mux_out: std_logic;
+	begin				
+	process(CLK, selector(0), selector)
 	begin
-		qsr <= ((not R) and qsr) or S;
-		qjk <= ((not K) and qjk) or (J and (not qjk));
-		qd  <= D;
-		qt  <= ((not T) and qt) or (T and (not qt));
-	process(CLK, selector(0))
-	begin
-		if selector(0) = '1' then	
-			 qjk <= '0';
-			 qsr <= '0';
-			 qd <= '0';
-			 qt <= '0';
+		if selector(0) = '1' then 
+			qjk	<= '0';
+			qt	<= '0';
+			qd	<= '0';
+			qsr	<= '0';
 	 	elsif CLK'event and CLK='1' then
-			--selector <= "100";
-			case(selector)is
-				when "000" => mux_out <= qjk;
-				when "001" => mux_out <= qt;
-				when "010" => mux_out <= qd;
-				when "011" => mux_out <= qsr;
-				when others => mux_out <= '0';
-			end case;
-	
-			if mux_out = '1' then
-					display <= "011000";
-			else			
-					display <= "111111";
- 		    end if;
+			qjk <= ((not K) and qjk) or (J and(not qjk));
+			qt  <= ((not T) and qt) or (T and (not qt));
+			qd  <= D;
+			qsr	<= ((not R) and qsr) or S;
 		end if;
-	end process;
+	case selector is
+		when "000" => mux_out <= qjk;
+		when "001" => mux_out <= qt;
+		when "010" => mux_out <= qd;
+		when "011" => mux_out <= qsr;
+		when others => mux_out <= '0';
+	end case;
+	if(mux_out) = '1' then
+		display <="011000";
+	 else
+	 	display <="111111";
+	end if;
+end process;
 end aff;
